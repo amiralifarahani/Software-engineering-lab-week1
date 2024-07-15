dd3e23b401d7569cd58e43551d9a0d95792dd592
AmirHosseinBagheriJebeli <amirhosseinbagheri0077@gmail.com>
2024-07-15 10:56:20 +0330

    Add video function

--- scripts/video.js
+++ scripts/video.js
@@ -6,3 +6,20 @@ const defaultOptions = {
     hideControlsOnPlay: true,
     progressColor: "red"
 };
+
+function Video(options = defaultOptions) {
+
+    // check options and its validity
+    if (!options || typeof options !== "object" || !options.wrapperID)
+        throw new Error("Options not passed to the Video functions.");
+    this.options = Object.assign(defaultOptions, options);
+
+    // check the existence of video wrapper
+    let wrapper = document.getElementById(this.options.wrapperID);
+    if (!wrapper && (!wrapper instanceof Element || !wrapper instanceof HTMLDocument))
+        throw new Error("Wrapper must be a valid node.");
+    this.wrapper = wrapper;
+
+    // initialize the video
+    this.init();
+}
