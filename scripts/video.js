2392ebff4af284883fa33c8474e9354634932f40
AmirHosseinBagheriJebeli <amirhosseinbagheri0077@gmail.com>
2024-07-15 11:04:11 +0330

    Add initial prototype

--- scripts/video.js
+++ scripts/video.js
@@ -23,3 +23,44 @@ function Video(options = defaultOptions) {
     // initialize the video
     this.init();
 }
+
+Video.prototype.init = function () {
+    if (this.video)
+        return;
+
+    // init the icons
+    this.icons = {
+        stop: '<svg><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect></svg>',
+        play: '<svg><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>',
+        pause: '<svg><rect x="6" y="4" width="4" height="16"></rect><rect x="14" y="4" width="4" height="16"></rect></svg>',
+        muted: '<svg><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>',
+        volume_0: '<svg><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon></svg>',
+        volume_1: '<svg><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>',
+        volume_2: '<svg><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>',
+        fullscreen: '<svg><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>',
+        exitFullscreen: '<svg><polyline points="4 14 10 14 10 20"></polyline><polyline points="20 10 14 10 14 4"></polyline><line x1="14" y1="10" x2="21" y2="3"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>',
+    };
+
+    // add special class to the wrapper to active all styles
+    this.wrapper.classList.add("m-video");
+
+    // check the existence of video tag in wrapper.
+    this.video = this.wrapper.querySelector("video");
+    if (!this.video) {
+        if (!this.options.videoSrc)
+            throw new Error("Can not find video tag in the wrapper or videoSrc in options");
+
+        // create video tag with source and append it to wrapper
+        this.generateVideoTag();
+    }
+
+    // se default controls to false;
+    this.video.controls = false;
+
+    // create custom controls element and append it to wrapper
+    this.generateControls();
+
+    // initialize the listeners.
+    this.initListeners();
+};
+
