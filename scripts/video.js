7dadc457cc48b23df0b9e7cca2d0b99e608dd4cf
AmirHosseinBagheriJebeli <amirhosseinbagheri0077@gmail.com>
2024-07-15 10:55:21 +0330

    Add default options

---
+++ scripts/video.js
@@ -0,0 +1,8 @@
+const defaultOptions = {
+    wrapperID: "video-wrapper",
+    videoSrc: null,
+    posterSrc: null,
+    absolute: false,
+    hideControlsOnPlay: true,
+    progressColor: "red"
+};
